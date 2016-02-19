33d750812ebac3eedd5204c3489efb73327f5632
Michael Robert Lee <leemr2@mcmaster.ca>
2016-02-19 00:34:48 -0500

    Most of GUI finished and functional
    Most of the GUI stuff has been finished.  All that's left to do is the
    creation and implementation of node class to represent points to place
    tokens (pieces) like nabil and I talked about.  Other than that, error
    checking and error messaging still need to be implemented (if the pieces
    have been placed correctly).

--- src/BoardView.java
+++ src/BoardView.java
@@ -4,27 +4,40 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class BoardView extends JPanel {
-    private BufferedImage image;
+    private BufferedImage image;    //board's image
+    private ArrayList<Token> tokens = new ArrayList<>(); //array of tokens, may replace with nodes, idk
 
-    public BoardView (){
+    public BoardView (){ //loads board
         try {
             image = ImageIO.read(new File("board.png"));
         }catch (IOException e){
             add (new JLabel("Board could not be loaded"));
         }
+        setBorder(BorderFactory.createLineBorder(Color.black));
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) { //draws board and tokens
         super.paintComponent(g);
         int dimension=(getWidth()<getHeight())?getWidth():getHeight();
-        Graphics2D util = (Graphics2D)g;
-        util.translate(this.getWidth()/2, this.getHeight()/2);
-        util.translate(-dimension/2, -dimension/2);
-        g.drawImage(image, 0, 0, dimension,dimension,null); // see javadoc for more info on the parameters
+        g.drawImage(image, getWidth()/2-dimension/2, 0, dimension,dimension,null); // see javadoc for more info on the parameters
+        for (Token t:tokens) {
+            g.setColor(new Color(t.getColour()));
+            g.fillOval(t.getX()-dimension/20,t.getY()-dimension/20,dimension/10,dimension/10);
+        }
+
     }
+
+
+    public void placeToken(Token newToken){ //place new token
+        tokens.add(newToken);
+        repaint();
+    }
+
+
 
 }
 
---
+++ src/Player.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * Created by mooyl on 2016-02-18.
+ */
+public class Player {
+    private int numTokens; //number of tokens
+    private int colour; //colour
+
+    public Player(int colour, int numTokens){
+        this.numTokens=numTokens;
+        this.colour=colour;
+    }
+
+    public void addToken (){ //add a token
+        numTokens++;
+    }
+
+    public boolean removeToken (){ //remove a token (also returns if it was able to remove a token)
+        if (numTokens>0){
+            numTokens--;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getColour(){ //get player's colour
+        return colour;
+    }
+
+    public int getNumTokens(){ //get number of tokens player has
+        return numTokens;
+    }
+
+}
