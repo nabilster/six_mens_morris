312c5cc16f0f1299f0541be9e1e7a64658dab4f8
nabilster <nabilsakran@hotmail.com>
2016-02-21 06:39:43 -0500

    Error Checking and Validation
    Created button for Checking, checks for stacked pieces and for less
    than 3 pieces on board no matter the player

--- src/GUI.java
+++ src/GUI.java
@@ -8,10 +8,10 @@ import java.util.Random;
 
 
 public class GUI extends JFrame implements ActionListener,MouseListener{
-
+    
     private BoardView board = new BoardView();              //view for board
     private  String colour1 = "red", colour2 = "blue";      //colour names
-    private int colour1Code = 16711680, colour2Code= 255;   //hexcodes for colours
+    private int colour1Code = 16711680, colour2Code= 255, colour3Code= 179255179;  //hexcodes for colours
     private JLabel turnIndicator = new JLabel("",JLabel.CENTER);   //label for whose turn
     private JPanel footer = new JPanel();                   //footer panel
     private int turnNumber;                                 //keeps track of whose turn it is
@@ -20,11 +20,12 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
     private Player [] players = new Player[2];              //player model
     private PlayerView [] playerViews=new PlayerView[2];    //player view
     private Node [] nodes = new Node [16];
-
-
+    
+    
+    
     public GUI (){
         initializeNodes();
-
+        
         //initializes stuff, adds listeners, sets first player
         header.setLayout(new FlowLayout());
         JButton newGame = new JButton("New Game");
@@ -38,14 +39,14 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         Random rng = new Random();
         turnNumber= rng.nextInt(2);
         updateTurn();
-
+        
         //sets up some panel spacing and layout
         left.setLayout(new BoxLayout(left, BoxLayout.PAGE_AXIS));
         right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
         left.add(Box.createRigidArea(new Dimension(100, 0)));
         right.add(Box.createRigidArea(new Dimension(100, 0)));
         footer.add(Box.createRigidArea(new Dimension(0,50)));
-
+        
         //adds panels
         setTitle("Six Men's Morris");
         board.addMouseListener(this);
@@ -56,35 +57,43 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         getContentPane().add(right,BorderLayout.EAST);
         getContentPane().add(footer,BorderLayout.SOUTH);
     }
-
+    
     //these are now relative to the center of the board panel due to the fact that the board image is centered with the
-        //panel, this is the easier way for tracking,  although is does cause some more complicated math
+    //panel, this is the easier way for tracking,  although is does cause some more complicated math
     private void initializeNodes(){
         nodes[0]=new Node(-0.412,0.093);
         nodes[1]=new Node(-0.002,0.093);
         nodes[2]=new Node(0.408,0.093);
-
+        
         nodes[3]=new Node(-0.207,0.298);
         nodes[4]=new Node(-0.002,0.298);
         nodes[5]=new Node(0.203,0.298);
-
+        
         nodes[6]=new Node(-0.412,0.503);
         nodes[7]=new Node(-0.207,0.503);
         nodes[8]=new Node(0.203,0.503);
         nodes[9]=new Node(0.408,0.503);
-
+        
         nodes[10]=new Node(-0.207,0.708);
         nodes[11]=new Node(-0.002,0.708);
         nodes[12]=new Node(0.203,0.708);
-
+        
         nodes[13]=new Node(-0.412,0.913);
         nodes[14]=new Node(-0.002,0.913);
         nodes[15]=new Node(0.408,0.913);
     }
-
-    //sets up side panels
+    
+    
+    //sets up side and bottom panels
     private void initSidePanels(){
         if (gameState.equals("New")){ //if new game, sets up labels
+            
+            JButton check= new JButton("Check");   // add check button to run check on gamestate
+            check.setActionCommand("check");
+            check.setAlignmentX(Component.CENTER_ALIGNMENT);
+            check.addActionListener(this);
+            footer.add(check);
+            
             JLabel player1Lable = new JLabel(colour1,JLabel.CENTER), player2Lable = new JLabel(colour2,JLabel.CENTER);
             player1Lable.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
             player2Lable.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
@@ -93,15 +102,20 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
             left.add(player1Lable);
             right.add(player2Lable);
         }else if (gameState.equals("Old")) { //if placing tokens, sets up buttons
-            JButton player1Button = new JButton(colour1), player2Button = new JButton(colour2);
+            
+            JButton player1Button = new JButton(colour1), player2Button = new JButton(colour2), check= new JButton("Check");;
             player1Button.setActionCommand("player1");
             player2Button.setActionCommand("player2");
+            check.setActionCommand("check");
             player1Button.setAlignmentX(Component.CENTER_ALIGNMENT);
             player2Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+            check.setAlignmentX(Component.CENTER_ALIGNMENT);
             player1Button.addActionListener(this);
             player2Button.addActionListener(this);
+            check.addActionListener(this);
             left.add(player1Button);
             right.add(player2Button);
+            footer.add(check);
         }
         //initializes player view and model
         players[0]=new Player(colour1Code,6);
@@ -111,7 +125,7 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         left.add(playerViews[0]);
         right.add(playerViews[1]);
     }
-
+    
     private void updateTurn(){
         //sets font colour and changes JLabel's text to reflect turn
         String text="<html><font color='";
@@ -121,7 +135,7 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         turnIndicator.setText(text);
         turnIndicator.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
     }
-
+    
     public void actionPerformed (ActionEvent e){ //if buttons are pressed
         if (e.getActionCommand().equals("NewG")||e.getActionCommand().equals("OldG")){
             //updates which mode to start
@@ -141,44 +155,67 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
             updateTurn();
             revalidate();
         }
+        else if (e.getActionCommand().equals("check")){   //when clicked run check
+            if(gameState.equals("New")){
+                checkScreen("New");
+            }
+            else{
+                checkScreen("Old");
+            }
+        }
     }
-
+    
+    
+    
     //the following are for mouse location.  Only matters where mouse is released
     @Override
     public void mouseClicked(MouseEvent e) {
     }
-
+    
     @Override
     public void mouseEntered(MouseEvent e) { }
-
+    
     @Override
     public void mouseExited(MouseEvent e) { }
-
+    
     @Override
     public void mousePressed(MouseEvent e) {
     }
-
+    
     @Override
     public void mouseReleased(MouseEvent e) {
         int x=e.getX();
         int y=e.getY();
+        
         int nodeNumber = findNodeNumber(x,y);
+        
+        
         if (e.getButton()==1&&!gameState.equals("")&&nodeNumber!=-1){           //if the correct mouse button is pressed and pieces are
-                                                                    //being placed
-            //TODO send coordinates to model to check for node and such
+            //being placed
+            
+            
             if (turnNumber%2==0){                   //if "red" player's turn
+                
                 if (players[0].removeToken()){
                     nodes[nodeNumber].addToken(colour1Code);
+                    
                     board.placeToken(nodes[nodeNumber]);
                     playerViews[0].removeToken();
+                    
+                    
                     //System.out.print("\tplaced red\t");
                 }
-            }else{                                  //if "blue" player's turn
+            }else {                                  //if "blue" player's turn
+                
                 if (players[1].removeToken()){
                     nodes[nodeNumber].addToken(colour2Code);
+                    
                     board.placeToken(nodes[nodeNumber]);
                     playerViews[1].removeToken();
+                    
+                    
                     //System.out.print("\tplaced blue\t");
+                    
                 }
             }
             if (!gameState.equals("Old"))turnNumber++; //if the next player goes
@@ -190,7 +227,7 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         }
         System.out.println("Board Width: "+board.getWidth()+" x: "+x+"\tBoard Height: "+board.getHeight()+" y: "+y);
     }
-
+    
     private int findNodeNumber (int x, int y){
         int nodeNumber=-1;
         double relX = (x-(board.getWidth()/2.0))/board.getDimension();
@@ -200,7 +237,43 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         System.out.print("Node Number: "+nodeNumber+"\t");
         return nodeNumber;
     }
-
+    
+    
+    
+    
+    //validation for tokenstacks
+    private boolean checkScreen(String gamestate){
+        
+        for (int i=0; i<nodes.length;i++){
+            if (nodes[i].getNumberTokens() >1){
+                nodes[i].addToken(colour3Code); // adds purple color token if more than one to indicate location
+                board.placeToken(nodes[i]);
+                JOptionPane.showMessageDialog(null, "Pieces Stacked (Highlighted in purple): Choose another spot  ", "Fail", JOptionPane.INFORMATION_MESSAGE);
+                return true;
+            }
+        }
+        boolean test1= players[1].getNumTokens() ==6 && players[1].getNumTokens() >=3;
+        boolean test2= players[1].getNumTokens() ==5 && players[1].getNumTokens() >=3;
+        boolean test3= players[1].getNumTokens() ==4 && players[1].getNumTokens() >=3;
+        boolean test4= players[1].getNumTokens() ==3 && players[1].getNumTokens() >3;
+        
+        if (test1|test2|test3|test4 && gamestate != "New"){
+            JOptionPane.showMessageDialog(null, "Not Enough Pieces on Board: Need to add more pieces to play", "Fail", JOptionPane.INFORMATION_MESSAGE);
+            return true;
+        }
+        if (gamestate== "New"){
+            JOptionPane.showMessageDialog(null, "Continue placing pieces until board is full", "Successful", JOptionPane.INFORMATION_MESSAGE);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Your Ready To Start Playing :)", "Successful", JOptionPane.INFORMATION_MESSAGE);
+            
+        }
+        return false;
+        
+    }
+    
+    
+    
     public static void main (String [] args){
         //sets the GUI look based on OS
         try {
@@ -214,6 +287,6 @@ public class GUI extends JFrame implements ActionListener,MouseListener{
         gui.setSize(defaultSize);
         gui.setMinimumSize(defaultSize);
         gui.setVisible(true);
-
+        
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,37 +1,42 @@
 import java.util.ArrayList;
 
-/**
- * Created by mooyl on 2016-02-20.
- */
 public class Node {
     private ArrayList<Token> tokens=new ArrayList<>();
     private final double relX;
     private final double relY;
-
+    private int counter;
+    //private Integer[] colourToken= new Integer[16];
+    
     public Node (double relX, double relY){
         this.relX=relX;
         this.relY=relY;
     }
-
+    
     public double getRelX(){
         return relX;
     }
-
+    
     public double getRelY (){
         return relY;
     }
-
+    
     public void addToken(int colour){
         this.tokens.add(new Token(colour));
+        
+        counter++;
     }
 
     public Token getTopToken(){
         return tokens.get(tokens.size()-1);
     }
-
+    
+    public int getNumberTokens(){
+        return counter;
+    }
+    
     public boolean inRange (double x, double y){
         if (x<1) return ((relX-0.05)<x&&x<(relX+0.05))&&((relY-0.05)<y&&y<(relY+0.05));
         return ((relX-0.05)<x&&x<(relX+0.05))&&((relY-0.05)<y&&y<(relY+0.05));
     }
-
+    
 }
